fb58cf56a6935d5e1bda2d23311354c93eccfe5d
Bianca <84011677+TripleBdesign@users.noreply.github.com>
2021-06-02 13:45:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//een functie definen of schrijven
+
+const pizzaMaken = function ( ){
+    console.log ("deegMaken");
+    console.log ("pizzaBeleggen");
+    console.log ("pizzaBakken");
+}
+
+//een functie gebruiken "calling" (als je dit niet doet dan wordt de function niet gebruikt)
+pizzaMaken ();
+pizzaMaken ();
+pizzaMaken ();
+
+
+
+const sushiMaken = function ( ){
+    console.log ("rijstKoken");
+    console.log ("groentenSnijden");
+    console.log ("rijstOpNori");
+    console.log ("groetenOfVisOpRijst");
+    console.log ("sushiRollen");
+    console.log ("sushiSnijden");
+}
+
+sushiMaken ();
+sushiMaken ();
+sushiMaken ();
+pizzaMaken ();
+pizzaMaken ();
